e5fa9a3591ec859b1027f072f9c0df5274ecaadf
Shane Moo <obries6@rpi.edu>
2017-11-29 16:47:22 -0500

    Fixed front page and player spacing

--- _120Stat/stats/forms.py
+++ _120Stat/stats/forms.py
@@ -1,5 +1,5 @@
 from django import forms
     
 class PlayerForm(forms.Form):
-    player_name = forms.CharField()
+    player_name = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': 'Player Name'}))
     
